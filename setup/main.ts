7ea1c2fdec28ba0e6df9f0eccca4c4d6175fe5bc
Samuel Huang <hi@sghuang.com>
2025-01-31 17:35:11 -0500

    feat: Inject Naive UI style to `<head>` (#7)
    * Inject naive ui style meta tag
    
    * Delete unneeded generate script
    
    * Bump version

--- setup/main.ts
+++ setup/main.ts
@@ -3,4 +3,12 @@ import { defineAppSetup } from "@slidev/types";
 
 export default defineAppSetup(({ app }) => {
   app.use(naive);
+  if (typeof window !== "undefined") {
+    const head = document.head || document.getElementsByTagName("head")[0];
+    ["naive-ui-style", "vueuc-style"].forEach(name => {
+      const meta = document.createElement("meta");
+      meta.name = name;
+      head.appendChild(meta);
+    });
+  }
 });
