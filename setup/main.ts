5319537e432b08a940649339ddafd5e6c5f0c8d1
Samuel Huang <hi@sghuang.com>
2025-02-03 22:04:57 -0500

    Format existing code

--- setup/main.ts
+++ setup/main.ts
@@ -1,5 +1,7 @@
 import { ref, computed, type Ref } from "vue";
 import { defineAppSetup } from "@slidev/types";
+import naive, { darkTheme, lightTheme } from "naive-ui";
+
 function useDarkTheme(): Ref<boolean> {
   const isDark = ref(false);
 
@@ -23,10 +25,14 @@ function useDarkTheme(): Ref<boolean> {
 }
 
 export default defineAppSetup(({ app }) => {
+  // Set up naive-ui
   app.use(naive);
+
+  // TODO: can this be done by importing CSS?
+  // Inject Naive UI styles into <head> element
   if (typeof window !== "undefined") {
     const head = document.head || document.getElementsByTagName("head")[0];
-    ["naive-ui-style", "vueuc-style"].forEach(name => {
+    ["naive-ui-style", "vueuc-style"].forEach((name) => {
       const meta = document.createElement("meta");
       meta.name = name;
       head.appendChild(meta);
