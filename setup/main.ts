47c997500884f081e9be072fb0822429ef4d30fb
Samuel Huang <hi@sghuang.com>
2025-02-03 22:29:02 -0500

    chore: minor tweaks to main.ts (#10)
    * Move brace around
    
    * Minor tweaks to style markers import
    
    * Bump version

--- setup/main.ts
+++ setup/main.ts
@@ -28,28 +28,27 @@ export default defineAppSetup(({ app }) => {
   // Set up naive-ui
   app.use(naive);
 
-  // TODO: can this be done by importing CSS?
   // Inject Naive UI styles into <head> element
   if (typeof window !== "undefined") {
-    const head = document.head || document.getElementsByTagName("head")[0];
+    const head = document.head;
     ["naive-ui-style", "vueuc-style"].forEach((name) => {
       const meta = document.createElement("meta");
       meta.name = name;
       head.appendChild(meta);
     });
-
-    // Manually inject n-config-provider context
-    const isDark = useDarkTheme();
-    /**
-     * !!! IMPORTANT !!!
-     * Missing properties here can cause misbehavior of components
-     * If components behave unexpectedly, check n-config-provider properties first
-     * This is defined in src/config-provider/ConfigProvider.ts
-     */
-    app.provide("n-config-provider", {
-      mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
-      mergedThemeOverridesRef: ref(), // required
-      mergedClsPrefixRef: ref("n"), // required for styles
-    });
   }
+
+  // Manually inject n-config-provider context
+  const isDark = useDarkTheme();
+  /**
+   * !!! IMPORTANT !!!
+   * Missing properties here can cause misbehavior of components
+   * If components behave unexpectedly, check n-config-provider properties first
+   * This is defined in src/config-provider/ConfigProvider.ts
+   */
+  app.provide("n-config-provider", {
+    mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
+    mergedThemeOverridesRef: ref(), // required
+    mergedClsPrefixRef: ref("n"), // required for styles
+  });
 });
