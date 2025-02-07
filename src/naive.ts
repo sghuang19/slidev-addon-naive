38b286be44633fda9347a25fc6e93675cd3c9b04
Samuel Huang <hi@sghuang.com>
2025-02-07 17:11:40 -0500

    Remove useBaseFontSizes from naive.ts

--- src/naive.ts
+++ src/naive.ts
@@ -1,14 +1,14 @@
+import { type App } from "vue";
+
 import naive from "naive-ui";
 
 import useConfigProvider from "./useConfigProvider.ts";
 import useInjectStyle from "./useInjectStyle.ts";
-import useBaseFontSizes from "./useBaseFontSizes.ts";
 
 export default {
-  install(app) {
+  install(app: App) {
     app.use(naive); // setup Naive UI
     useInjectStyle(); // inject styles to <head>
     useConfigProvider(app); // provide theme and overrides
-    useBaseFontSizes();
   },
 };
