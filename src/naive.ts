b7d22f5dd1d9361f142780b1112f0015512b6572
Samuel Huang <hi@sghuang.com>
2025-02-07 10:53:54 -0500

    Abstract useConfigProvider

--- src/naive.ts
+++ src/naive.ts
@@ -1,7 +1,7 @@
 import { ref, computed } from "vue";
 import naive, { darkTheme, lightTheme } from "naive-ui";
 
-import useDarkTheme from "./useDarkTheme";
+import useConfigProvider from "./useConfigProvider";
 
 export default {
   install(app) {
@@ -18,12 +18,6 @@ export default {
       });
     }
 
-    // Manually inject n-config-provider context
-    const isDark = useDarkTheme();
-    app.provide("n-config-provider", {
-      mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
-      mergedThemeOverridesRef: ref({}), // required
-      mergedClsPrefixRef: ref("n"), // required for styles
-    });
+    useConfigProvider(app);
   },
 };
---
+++ src/useConfigProvider.ts
@@ -0,0 +1,16 @@
+import { ref, computed } from "vue";
+import { darkTheme, lightTheme } from "naive-ui";
+
+import useDarkTheme from "./useDarkTheme";
+
+const useConfigProvider = (app) => {
+  // Manually inject n-config-provider context
+  const isDark = useDarkTheme();
+  app.provide("n-config-provider", {
+    mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
+    mergedThemeOverridesRef: ref({}), // required
+    mergedClsPrefixRef: ref("n"), // required for styles
+  });
+};
+
+export default useConfigProvider;
