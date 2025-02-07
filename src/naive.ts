aea618cc037a2fd9512ebdbe357283e1ead0a3fa
Samuel Huang <hi@sghuang.com>
2025-02-07 10:57:02 -0500

    Abstract useInjectStyle.ts

--- src/naive.ts
+++ src/naive.ts
@@ -1,23 +1,12 @@
-import { ref, computed } from "vue";
-import naive, { darkTheme, lightTheme } from "naive-ui";
+import naive from "naive-ui";
 
 import useConfigProvider from "./useConfigProvider";
+import useInjectStyle from "./useInjectStyle";
 
 export default {
   install(app) {
-    // Set up naive-ui
-    app.use(naive);
-
-    // Inject Naive UI styles into <head> element
-    if (typeof window !== "undefined") {
-      const head = document.head;
-      ["naive-ui-style", "vueuc-style"].forEach((name) => {
-        const meta = document.createElement("meta");
-        meta.name = name;
-        head.appendChild(meta);
-      });
-    }
-
-    useConfigProvider(app);
+    app.use(naive); // setup Naive UI
+    useInjectStyle(); // inject styles to <head>
+    useConfigProvider(app); // provide theme and overrides
   },
 };
