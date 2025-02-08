4a5425387d15e1af056feb1b7d84a45b91a95e8e
Samuel Huang <hi@sghuang.com>
2025-02-07 19:12:12 -0500

    Use ResizeObserver for font sizes

--- src/useBaseFontSize.ts
+++ src/useBaseFontSize.ts
@@ -8,7 +8,7 @@ const setBaseFontSize = () => {
   if (element) {
     const computedStyle = window.getComputedStyle(element);
     fontSize.value = computedStyle.fontSize;
-    console.debug("[Naive] Base font size:", fontSize.value);
+    console.debug("[Naive] Base font size updated:", fontSize.value);
   } else {
     console.error(
       "[Naive] .slidev-layout not found, using default font size (16px)",
@@ -16,16 +16,33 @@ const setBaseFontSize = () => {
   }
 };
 
-const observeSlidevLayout = () => {
-  // Setup observer for DOM changes
+const observeBaseFontSize = () => {
+  // Setup observer for DOM changes and font size changes
   const layoutObserver = new MutationObserver(() => {
-    const slidevLayout = document.querySelector(".slidev-layout");
-
-    if (slidevLayout) {
-      console.debug("[Naive] Found .slidev-layout");
-      layoutObserver.disconnect(); // stop observing the document body
-      setBaseFontSize();
+    const layout = document.querySelector(".slidev-layout");
+    if (!layout) {
+      return;
     }
+
+    console.debug("[Naive] Found .slidev-layout, switching to style observer");
+    layoutObserver.disconnect(); // stop observing the document body
+
+    // Create new observers to get updated font size
+    const sizeObserver = new ResizeObserver(setBaseFontSize);
+    // somehow needed to get styles from UnoCSS @apply directives
+    sizeObserver.observe(layout);
+
+    const styleObserver = new MutationObserver(setBaseFontSize);
+    // needed to get styles update in runtime
+    styleObserver.observe(layout, {
+      attributes: true,
+      attributeFilter: ["style"],
+    });
+
+    // TODO: this won't change font size in runtime. a polling approach can be
+    // used, but I'm hesitating because of performance concerns
+
+    setBaseFontSize(); // make an attempt to set the font size
   });
 
   // Start observing the document body for layout appearance
@@ -35,14 +52,14 @@ const observeSlidevLayout = () => {
   });
 
   console.debug("[Naive] Watching for .slidev-layout element to show up");
-}
+};
 
 if (typeof window === "undefined") {
   console.error(
     `[Naive] Window is undefined, can't observe DOM to set base font size`,
   );
 } else {
-  observeSlidevLayout();
+  observeBaseFontSize();
 }
 
 export default fontSize;
