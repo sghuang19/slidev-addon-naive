6e26cc008ff8e41c923a9ac2d18ea715dbe42e22
Samuel Huang <hi@sghuang.com>
2025-02-13 22:55:36 -0500

    Use multiplier and default font sizes

--- src/useStyles.ts
+++ src/useStyles.ts
@@ -1,24 +1,29 @@
-import { computed, watch } from "vue";
+import { type ComputedRef, computed, watch } from "vue";
 
 import baseFontSize from "./useBaseFontSize.ts";
-import { deriveSize } from "./utils.ts";
+import { deriveSize, getMultiplier } from "./utils.ts";
+
+const multiplier: ComputedRef<number> = computed(() =>
+  getMultiplier("14px", baseFontSize.value),
+);
 
 // see https://naiveui.com/en-US/os-theme/docs/theme#use-theme-vars
-const fontSizeMultipliers = {
-  tiny: 0.75,
-  small: 0.875,
-  default: 1,
-  medium: 1,
-  large: 1.125,
-  huge: 1.25,
-};
+const DEFAULT_FONT_SIZES = {
+  "": "14px",
+  mini: "12px",
+  tiny: "12px",
+  small: "14px",
+  medium: "14px",
+  large: "15px",
+  huge: "16px",
+} as const;
 
 /** Theme overrides injected to Naive context */
 export const themeOverrides = computed(() => {
   const derivedFontSizes = Object.fromEntries(
-    Object.entries(fontSizeMultipliers).map(([variant, multiplier]) => [
-      `fontSize${variant === "default" ? "" : variant[0].toUpperCase() + variant.slice(1)}`,
-      deriveSize(baseFontSize.value, multiplier),
+    Object.entries(DEFAULT_FONT_SIZES).map(([variant, size]) => [
+      `fontSize${variant}`,
+      deriveSize(size, multiplier.value),
     ]),
   );
 
@@ -29,10 +34,10 @@ export const themeOverrides = computed(() => {
 
 const setStyles = () => {
   // set font size variants
-  Object.entries(fontSizeMultipliers).forEach(([variant, multiplier]) => {
+  Object.entries(DEFAULT_FONT_SIZES).forEach(([variant, size]) => {
     document.documentElement.style.setProperty(
-      `--n-font-size${variant === "default" ? "" : `-${variant}`}`,
-      deriveSize(baseFontSize.value, multiplier),
+      `--n-font-size${variant && `-${variant}`}`,
+      deriveSize(size, multiplier.value),
     );
   });
 };
