8898989cd0202cbed5de2610cf96f4099ecff853
Samuel Huang <hi@sghuang.com>
2025-02-14 15:40:08 -0500

    Let extractor take path

--- src/extractor.sh
+++ src/extractor.sh
@@ -1,12 +1,14 @@
+SLIDES_PATH=${1:-"slides.md"}
+
 # 1. Extract open tags of Naive UI components (starting with N and followed by
 #    an uppercase letter)
 # 2. Remove duplicates
 # 3. Remove prefixes and suffixes
 # 4. Remove duplicates
 
-grep -o '<N[A-Z][a-zA-Z]*[ />]' ../slides.md \
+grep -o "<N[A-Z][a-zA-Z]*[ />]" "$FILE_PATH" \
   | sort \
   | uniq \
-  | sed 's/^<\(.*\)[ />]$/\1/' \
+  | sed "s/^<\(.*\)[ />]$/\1/" \
   | sort \
   | uniq
