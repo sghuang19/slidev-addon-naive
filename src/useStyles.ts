7ebf0f423e383d260cf50f317742e4f788f2ef68
Samuel Huang <hi@sghuang.com>
2025-02-15 12:54:32 -0500

    fix: dynamic icon size for alerts (#38)
    * Create parseSize()
    
    * Refactor deriveSize()
    
    * Add getMultiplier()
    
    * Use multiplier and default font sizes
    
    * Better typing
    
    * Sort imports
    
    * Minor improvements
    
    * Set icon size

--- src/useStyles.ts
+++ src/useStyles.ts
@@ -1,43 +1,69 @@
-import { computed, watch } from "vue";
+import { type ComputedRef, computed, watch } from "vue";
+
+import { type GlobalThemeOverrides } from "naive-ui";
 
 import baseFontSize from "./useBaseFontSize.ts";
-import { deriveSize } from "./utils.ts";
+import { deriveSize, getMultiplier } from "./utils.ts";
 
-// see https://naiveui.com/en-US/os-theme/docs/theme#use-theme-vars
-const fontSizeMultipliers = {
-  tiny: 0.75,
-  small: 0.875,
-  default: 1,
-  medium: 1,
-  large: 1.125,
-  huge: 1.25,
-};
-
-/** Theme overrides injected to Naive context */
-export const themeOverrides = computed(() => {
-  const derivedFontSizes = Object.fromEntries(
-    Object.entries(fontSizeMultipliers).map(([variant, multiplier]) => [
-      `fontSize${variant === "default" ? "" : variant[0].toUpperCase() + variant.slice(1)}`,
-      deriveSize(baseFontSize.value, multiplier),
-    ]),
-  );
-
-  console.debug("[Naive] Derived font sizes:", derivedFontSizes);
-
-  return { common: derivedFontSizes };
+const multiplier: ComputedRef<number> = computed(() => {
+  const newVal = getMultiplier("14px", baseFontSize.value);
+  console.debug(`[Naive] Multiplier set to ${newVal}`);
+  return newVal;
 });
 
+// see https://naiveui.com/en-US/os-theme/docs/theme#use-theme-vars
+const DEFAULT_FONT_SIZES = {
+  "": "14px",
+  mini: "12px",
+  tiny: "12px",
+  small: "14px",
+  medium: "14px",
+  large: "15px",
+  huge: "16px",
+} as const;
+
+/** Theme overrides injected to Naive context */
+export const themeOverrides: ComputedRef<GlobalThemeOverrides> = computed(
+  () => {
+    const derivedFontSizes = Object.fromEntries(
+      Object.entries(DEFAULT_FONT_SIZES).map(([variant, size]) => [
+        `fontSize${variant}`,
+        deriveSize(size, multiplier.value),
+      ]),
+    );
+
+    console.debug("[Naive] Derived font sizes:", derivedFontSizes);
+
+    return { common: derivedFontSizes };
+  },
+);
+
+/** Styles set directly in DOM */
 const setStyles = () => {
   // set font size variants
-  Object.entries(fontSizeMultipliers).forEach(([variant, multiplier]) => {
+  Object.entries(DEFAULT_FONT_SIZES).forEach(([variant, size]) => {
     document.documentElement.style.setProperty(
-      `--n-font-size${variant === "default" ? "" : `-${variant}`}`,
-      deriveSize(baseFontSize.value, multiplier),
+      `--n-font-size${variant && `-${variant}`}`,
+      deriveSize(size, multiplier.value),
     );
   });
+
+  // set icon size
+  const iconSize = deriveSize("24px", multiplier.value);
+  document.documentElement.style.setProperty(
+    "--n-icon-size",
+    `${iconSize} !important`,
+  );
+
+  // also needed since Alert has inline styles
+  const style = document.createElement("style");
+  style.textContent = `.n-alert { --n-icon-size: ${iconSize} !important; }`;
+  document.head.appendChild(style);
 };
 
-export default function useStyles() {
+const useStyles = () => {
   setStyles(); // set initial styles immediately
-  watch(baseFontSize, setStyles);
-}
+  watch(multiplier, setStyles);
+};
+
+export default useStyles;
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,35 @@
-export const deriveSize = (baseSize: string, multiplier: number): string => {
-  const match = baseSize.match(/^(\d+(?:\.\d+)?)(px|rem|em)$/);
+const parseSize = (size: string): { value: number; unit: string } | null => {
+  const match = size.match(/^(\d+(?:\.\d+)?)(px|rem|em)$/);
   if (!match) {
-    console.error(
-      "[Naive] Invalid CSS size, fall back to base size:",
-      baseSize,
-    );
-    return baseSize;
+    console.error(`[Naive] Invalid size string: ${size}`);
+    return null;
   }
+  return {
+    value: parseFloat(match[1]),
+    unit: match[2],
+  };
+};
 
-  const baseSizeValue = parseFloat(match[1]);
-  const unit = match[2];
-  const newSizeValue = Math.round(baseSizeValue * multiplier);
+export const deriveSize = (size: string, multiplier: number): string => {
+  const parsedSize = parseSize(size);
+  if (!parsedSize) {
+    console.error(`[Naive] Invalid CSS size ${size}, fall back to base size.`);
+    return size;
+  }
+  const { value, unit } = parsedSize;
+  return `${
+    unit === "px"
+      ? Math.round(value * multiplier) // round to integer for px
+      : Math.round(value * multiplier * 100) / 100 // round to 2 decimal places for rem | em
+  }${unit}`;
+};
 
-  return `${newSizeValue}${unit}`;
+export const getMultiplier = (baseSize: string, targetSize: string): number => {
+  const parsedBaseSize = parseSize(baseSize);
+  const parsedTargetSize = parseSize(targetSize);
+  if (!parsedBaseSize || !parsedTargetSize) {
+    console.error(`[Naive] Fall back to multiplier = 1`);
+    return 1;
+  }
+  return parsedTargetSize.value / parsedBaseSize.value;
 };
