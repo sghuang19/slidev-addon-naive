85cfedba2046cb0914dae4ad684df49f8ec04ccd
Samuel Huang <hi@sghuang.com>
2025-02-17 16:20:50 -0500

    Improved type definition

--- src/useComponents.ts
+++ src/useComponents.ts
@@ -1,11 +1,13 @@
-import type { App, DefineComponent, Plugin } from "vue";
+import type { App, DefineComponent } from "vue";
 
 import * as naive from "virtual:naive";
 
+const plugin = naive.default;
+
 const useComponents = (app: App) => {
-  if (naive.default) {
+  if (plugin) {
     console.warn("[Naive] Tree-shaking not available");
-    app.use(naive.default as Plugin<[]>);
+    app.use(plugin);
   } else {
     Object.entries(naive).forEach(([name, component]) => {
       app.component(name, component as DefineComponent);
