8f1bee9621f1152b9327f0418de88d174b8fdf0b
Samuel Huang <hi@sghuang.com>
2025-02-18 11:03:34 -0500

    add support for kebab case in shell script

--- src/extractor.sh
+++ src/extractor.sh
@@ -6,9 +6,13 @@ FILE_PATH=${1:-"slides.md"}
 # 3. Remove prefixes and suffixes
 # 4. Remove duplicates
 
-grep -o "<N[A-Z][a-zA-Z]*[ />]" "$FILE_PATH" \
+grep -o '<\(N[A-Z][a-zA-Z]*\|n-[a-z]\(-[a-z]\|[a-z]\)*\)[ />]' "$FILE_PATH" \
   | sort \
   | uniq \
-  | sed "s/^<\(.*\)[ />]$/\1/" \
+  | sed -E 's/^<(.*)[ />]$/\1/' \
+  | sed -E 's/^n/N/' \
+  | sed 's/-/ /g' \
+  | awk '{for (i=1; i<=NF; i++) $i=toupper(substr($i,1,1)) substr($i,2); print $0}' \
+  | sed 's/ //g' \
   | sort \
   | uniq
