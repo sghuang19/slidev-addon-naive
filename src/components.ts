dc1c536b85902574e735fb8bb09fb26d11393a1d
Samuel Huang <hi@sghuang.com>
2025-02-17 19:46:24 -0500

    Components as a plugin

--- src/components.ts
+++ src/components.ts
@@ -1,18 +1,18 @@
-import type { App, DefineComponent } from "vue";
+import { type Plugin } from "vue";
 
 import * as naive from "virtual:naive";
 
 const plugin = naive.default;
 
-const useComponents = (app: App) => {
+const components: Plugin = (app) => {
   if (plugin) {
     console.warn("[Naive] Tree-shaking not available");
     app.use(plugin);
   } else {
     Object.entries(naive).forEach(([name, component]) => {
-      app.component(name, component as DefineComponent);
+      app.component(name, component);
     });
   }
 };
 
-export default useComponents;
+export default components;
--- src/naive.ts
+++ src/naive.ts
@@ -1,13 +1,13 @@
 import { type Plugin } from "vue";
 
 import "../style.css";
-import useComponents from "./useComponents.ts";
+import components from "./components.ts";
 import useConfigProvider from "./useConfigProvider.ts";
 import useInjectStyle from "./useInjectStyle.ts";
 import useStyles from "./useStyles.ts";
 
 const naive: Plugin = (app) => {
-  useComponents(app); // register components
+  app.use(components); // register components
   useInjectStyle(); // inject styles to <head>
   useConfigProvider(app); // provide theme overrides
   useStyles(); // additional style adjustments
