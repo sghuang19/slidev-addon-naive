f806469f6bb30176b33a89b4d3bd384664e099be
Samuel Huang <hi@sghuang.com>
2025-02-22 23:32:25 -0500

    chore: move type declaration outside of src (#66)

--- types/virtual-naive.d.ts
+++ types/virtual-naive.d.ts
@@ -1,8 +1,8 @@
 /** Virtual module defined by Vite */
-
 declare module "virtual:naive" {
   export * from "naive-ui";
   import naive from "naive-ui";
   const defaultExport: typeof naive | undefined;
+  /** Export Naive UI plugin as default if tree-shaking not available */
   export default defaultExport;
 }
