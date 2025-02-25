55e6c98a36eddfcc3fb07b8f96aab64d08785287
Samuel Huang <hi@sghuang.com>
2025-02-24 23:06:49 -0500

    Add mergedNamespaceRef

--- src/config-provider.ts
+++ src/config-provider.ts
@@ -15,11 +15,16 @@ import isDark from "./useDarkMode";
 
 const configProvider: Plugin = (app) => {
   // Manually inject n-config-provider context
-  app.provide("n-config-provider", {
-    mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
-    mergedThemeOverridesRef: themeOverrides, // required
-    mergedClsPrefixRef: ref("n"), // required for styles
-  } as Partial<ConfigProviderInjection>);
+  app.provide(
+    "n-config-provider",
+    /* eslint sort-keys: "error" */
+    {
+      mergedClsPrefixRef: ref("n"), // required for styles
+      mergedNamespaceRef: ref(), // required for popovers to be rendered
+      mergedThemeOverridesRef: themeOverrides, // required
+      mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
+    } as Partial<ConfigProviderInjection>,
+  );
 };
 
 export default configProvider;
