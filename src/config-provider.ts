484e2ed03b844084dadb3e709a84823793fc58bc
Samuel Huang <hi@sghuang.com>
2025-02-26 13:12:26 -0500

    pass merged theme to config provider

--- src/config-provider.ts
+++ src/config-provider.ts
@@ -3,28 +3,27 @@ import { type Plugin, computed, ref } from "vue";
 import { darkTheme, lightTheme } from "naive-ui";
 import { ConfigProviderInjection } from "naive-ui/es/config-provider/src/internal-interface";
 
-import { themeOverrides } from "./styles";
+import config from "./useConfig";
 import isDark from "./useDarkMode";
 
 /**
- * I'm trying to mimic the design of original Naive UI, which can be found by
- * checking default theme variables or inspecting Naive UI website.
- *
- * https://www.naiveui.com/en-US/docs/theme
+ * Mimics the design of original Naive UI
+ * https://naiveui.com/en-US/os-theme/docs/theme
  */
 
+const { darkThemeOverrides, lightThemeOverrides, ...themeOverrides } =
+  config.value;
+
 const configProvider: Plugin = (app) => {
-  // Manually inject n-config-provider context
-  app.provide(
-    "n-config-provider",
-    /* eslint sort-keys: "error" */
-    {
-      mergedClsPrefixRef: ref("n"), // required for styles
-      mergedNamespaceRef: ref(), // required for popovers to be rendered
-      mergedThemeOverridesRef: themeOverrides, // required
-      mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)), // required
-    } as Partial<ConfigProviderInjection>,
-  );
+  /* eslint sort-keys: "error" */
+  app.provide<Partial<ConfigProviderInjection>>("n-config-provider", {
+    mergedClsPrefixRef: ref("n"),
+    mergedThemeOverridesRef: computed(() => ({
+      ...themeOverrides,
+      ...(isDark.value ? darkThemeOverrides : lightThemeOverrides),
+    })),
+    mergedThemeRef: computed(() => (isDark.value ? darkTheme : lightTheme)),
+  });
 };
 
 export default configProvider;
